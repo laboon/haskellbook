5bcae208ef8604a0d47a3bdadfe3d4f7d78c1296
laboon <laboon@gmail.com>
2017-09-07 15:08:49 -0400

    ch2 exercises

---
+++ ch2/ex25.hs
@@ -0,0 +1,12 @@
+-- Exercises from section 2.5
+
+-- Exercise 1
+-- These are entered into the GHCi REPL
+-- let half x = x / 2
+-- let square = x * x
+
+-- Exercise 2
+ex2 x = 3.14 * (x * x)
+
+-- Exercise 3
+ex3 x = pi * (x * x)
---
+++ ch2/ex27.hs
@@ -0,0 +1,14 @@
+-- Exercises from 2.7
+
+-- Exercise 1
+-- This should be entered into GHCi REPL.
+-- let area x = 3.14 * (x * x)
+
+-- Exercise 2
+-- This should be entered into GHCi REPL.
+--  let double x = x * 2
+
+-- Exercise 3
+x = 7
+y = 10
+f = x + y
